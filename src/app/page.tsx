3f5caef34fb24c52d70315f794b4db2ae63c36f6
patelvk2000 <patelvk2000@gmail.com>
2024-06-11 04:53:24 +0000

    Added UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import GenerateForm from "@/components/form";
 
 export default function Home() {
   return (
-    <main>
-      <GenerateForm />
+    <main className="bg-gray-950 min-h-screen flex w-full flex-col items-center justify-center ">
+        <GenerateForm />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    theme: {
+      extend: {},
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
 };
 export default config;
